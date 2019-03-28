42fb50d19275bbfaa74402b3786f48ac21649987
Julian <julian.a.garza11@gmail.com>
2019-03-28 21:44:57 +0000

    Changed form/email sending and styling

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -40,3 +40,26 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+    var name    = document.getElementById('inputName')
+    var email   = document.getElementById('inputEmail')
+    var message = document.getElementById('inputMessage')
+
+    if (!name.value || !email.value || !message.value) {
+      alertify.error("Please check your entries");
+      return false;
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '//formspree.io/julian.a.garza11@gmail.com',
+        data: $('#contact-form').serialize(),
+        datatype: 'json'
+      });
+      e.preventDefault();
+      $(this).get(0).reset();
+      alertify.success("Message sent");
+    }
+  });
+});
